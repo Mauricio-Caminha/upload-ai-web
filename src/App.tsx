ddad295f5cdc55b5b8e371bc359765ad8c9e6c4b
Maurício Caminha <mauricio.caminha@hotmail.com>
2023-09-12 11:58:01 -0300

    [refactor] base theme dark/violet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { Button } from "./components/ui/button"
+
 export function App() {
   return (
-    <h1>Hello, World!</h1>
+    <Button> Clique ai</Button>
+    // <h1>Hello, World!</h1>
   )
 }
